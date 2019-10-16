60847ac777e2839bf367977101d9fc25c746361b
Hugo Perdomo <hugo.j.dev@gmail.com>
2019-10-15 21:15:37 -0400

    Added routes for api/item 🤔

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,28 @@
 import { Router } from 'express'
 
 const router = Router()
+router
+  .route('/')
+  .get((req, res) => {
+    res.send('Hi')
+  })
+  .post((req, res) => {
+    res.send({ message: 'hi' })
+  })
+
+router
+  .route('/:id')
+  .get((req, res) => {
+    res.send('hello id')
+  })
+  .post((req, res) => {
+    res.send({ message: 'with id' })
+  })
+  .put((req, res) => {
+    res.send({ message: 'trying to put uh?' })
+  })
+  .delete((req, res) => {
+    res.send({ message: 'Attempting to delete...' })
+  })
 
 export default router
